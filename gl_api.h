842f8cad891c02b4e0911b3ca9c8ae84a8d239e7
yoharol <flameyeelf@gmail.com>
2022-01-24 13:14:15 +0800

    structure refactory

--- gl_api.h
+++ gl_api.h
@@ -1,6 +1,10 @@
+#ifndef GL_API
+#define GL_API
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <vector>
 
 class FPS_Counter
 {
@@ -27,49 +31,42 @@ class FPS_Counter
 
     int GetFPS()
     {
-        return count / (glfwGetTime() - start_time);
+        return static_cast<int>(count / (glfwGetTime() - start_time));
     }
 };
 
-void InitGlew()
+void InitGlew();
+
+GLFWwindow *CreateWindow(const int width, const int height, char *window_name);
+
+void UpdateWindow(GLFWwindow *window);
+
+void EndGL(GLFWwindow *window);
+
+template <typename VEC, typename SCALAR = typename VEC::Scalar>
+inline VEC world_to_screen(const int N, const int M, int x, int y, SCALAR halfdx, SCALAR halfdy)
 {
-    // Load glew
-    if (glewInit() != GLEW_OK)
+    VEC p = SCALAR(2.0) * VEC(SCALAR(x) / N + halfdx, SCALAR(y) / M + halfdy) + VEC(-1.0, -1.0);
+    return p;
+}
+
+template <typename VEC, typename SCALAR = typename VEC::Scalar>
+void DrawArray(const int N, const int M, const std::vector<SCALAR> &img)
+{
+    glClearColor(0.f, 0.f, 0.f, 1.f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glPointSize(1.);
+    glBegin(GL_POINTS);
+    for (int i = 0; i < N * M; i++)
     {
-        std::cerr << "GLEW init failed. It might be caused if the current context is not set.\n";
+        int x = i % N;
+        int y = i / N;
+        VEC p = world_to_screen<VEC>(N, M, x, y, 1.0 / SCALAR(N), 1.0 / SCALAR(M));
+        typename VEC::Scalar c = img[i];
+        glColor4f(c, 0., 0., (typename VEC::Scalar)x / N);
+        glVertex3f(p(0), p(1), 0);
     }
-    else
-        printf("GLFW and GLEW successfully initialized with OpenGL verison %s\n", glGetString(GL_VERSION));
+    glEnd();
 }
 
-GLFWwindow *CreateWindow(const int width, const int height, char *window_name)
-{
-    glfwInit();
-    GLFWwindow *window = glfwCreateWindow(width, height, window_name, NULL, NULL);
-    if (!window)
-    {
-        std::cerr << "Cannot create window\n";
-        glfwTerminate();
-        return NULL;
-    }
-    // Introduce window to current context
-    glfwMakeContextCurrent(window);
-
-    InitGlew();
-
-    glViewport(0, 0, width, height);
-
-    return window;
-}
-
-void UpdateWindow(GLFWwindow *window)
-{
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-}
-
-void EndGL(GLFWwindow *window)
-{
-    glfwDestroyWindow(window);
-    glfwTerminate();
-}
+#endif
--- main.cpp
+++ main.cpp
@@ -6,32 +6,6 @@
 using VEC = Eigen::Vector2f;
 using SCALAR = typename VEC::Scalar;
 
-template <typename VEC, typename SCALAR = typename VEC::Scalar>
-inline VEC world_to_screen(const int N, const int M, int x, int y, SCALAR halfdx, SCALAR halfdy)
-{
-    VEC p = SCALAR(2.0) * VEC(SCALAR(x) / N + halfdx, SCALAR(y) / M + halfdy) + VEC(-1.0, -1.0);
-    return p;
-}
-
-template <typename VEC, typename SCALAR = typename VEC::Scalar>
-void DrawArray(const int N, const int M, const std::vector<SCALAR> &img)
-{
-    glClearColor(0.f, 0.f, 0.f, 1.f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glPointSize(1.);
-    glBegin(GL_POINTS);
-    for (int i = 0; i < N * M; i++)
-    {
-        int x = i % N;
-        int y = i / N;
-        VEC p = world_to_screen<VEC>(N, M, x, y, 1.0 / SCALAR(N), 1.0 / SCALAR(M));
-        typename VEC::Scalar c = img[i];
-        glColor4f(c, 0., 0., (typename VEC::Scalar)x / N);
-        glVertex3f(p(0), p(1), 0);
-    }
-    glEnd();
-}
-
 int main()
 {
 
